3143ea709bb0ef3a4d94cb11b73676c06ed26c35
Dennis Appelt <dappelt@ripple.com>
2018-01-23 12:37:09 +0000

    test: don't throw if account associates the same paychan again

--- test/pluginSpec.js
+++ test/pluginSpec.js
@@ -36,32 +36,39 @@ describe('pluginSpec', () => {
 
   describe('handle custom data', () => {
     describe('channel protocol', () => {
-      it('does not race when assigning a channel to an account', () => {
-        const from = 'test.example.35YywQ-3GYiO3MM4tvfaSGhty9NZELIBO3kmilL0Wak'
-        const channelId = '45455C767516029F34E9A9CEDD8626E5D955964A041F6C9ACD11F9325D6164E0'
-        const channelSig = '9F878049FBBF4CEBAB29E6D840984D777C10ECE0FB96B0A56FF2CBC90D38DD03571A7D95A7721173970D39E1FC8AE694D777F5363AA37950D91F9B4B7E179C00'
-        const message = {
+      beforeEach(() => {
+        this.from = 'test.example.35YywQ-3GYiO3MM4tvfaSGhty9NZELIBO3kmilL0Wak'
+        this.channelId = '45455C767516029F34E9A9CEDD8626E5D955964A041F6C9ACD11F9325D6164E0'
+        this.channelSig = '9F878049FBBF4CEBAB29E6D840984D777C10ECE0FB96B0A56FF2CBC90D38DD03571A7D95A7721173970D39E1FC8AE694D777F5363AA37950D91F9B4B7E179C00'
+        this.channelProtocol = {
           data: {
             protocolData: [{
               protocolName: 'channel',
               contentType: 0,
-              data: Buffer.from(channelId, 'hex') },
+              data: Buffer.from(this.channelId, 'hex') },
             { protocolName: 'channel_signature',
               contentType: 0,
-              data: Buffer.from(channelSig, 'hex') }]
+              data: Buffer.from(this.channelSig, 'hex') }]
           }
         }
+      })
 
+      it('does not race when assigning a channel to an account', () => {
         const getStub = sinon.stub(this.plugin._store._store, 'get')
-        getStub.withArgs('channel:' + channelId).onFirstCall().callsFake(() => {
+        getStub.withArgs('channel:' + this.channelId).onFirstCall().callsFake(() => {
           // simulate another process writing to the cache while we wait for the store to return
-          this.plugin._store.set('channel:' + channelId, 'some_other_account')
+          this.plugin._store.set('channel:' + this.channelId, 'some_other_account')
           return Promise.resolve(null)
         })
 
-        return assert.isRejected(this.plugin._handleCustomData(from, message),
+        return assert.isRejected(this.plugin._handleCustomData(this.from, this.channelProtocol),
           'this channel has already been associated with a different account. ' + 
           'account=35YywQ-3GYiO3MM4tvfaSGhty9NZELIBO3kmilL0Wak associated=some_other_account')
+      })
+
+      it('don\'t throw if an account associates the same paychan again' , () => {
+        const sendChannelProof = () => this.plugin._handleCustomData(this.from, this.channelProtocol)
+        return assert.isFulfilled(Promise.all([sendChannelProof(), sendChannelProof()]))
       })
     })
   })
